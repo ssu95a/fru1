744aedd8fb0308a8b598d6ff79a2a25a5204534c
Sulimoff <ssu95a@gmail.com>
2026-06-16 20:21:15 +0300

    JAVAPRT-9 add edit form

--- src/main/java/ru/inversion/fru/print/altviewer/FruEditController.java
+++ src/main/java/ru/inversion/fru/print/altviewer/FruEditController.java
@@ -1,4 +1,18 @@
 package ru.inversion.fru.print.altviewer;
 
-public class FruEditController {
+import javafx.fxml.Initializable;
+import org.controlsfx.glyphfont.GlyphFont;
+import org.controlsfx.glyphfont.GlyphFontRegistry;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FruEditController implements Initializable {
+
+   final private GlyphFont fontAwesome = GlyphFontRegistry.font("FontAwesome");
+
+   @Override
+   public void initialize(URL location, ResourceBundle resources) {
+
+   }
 }
